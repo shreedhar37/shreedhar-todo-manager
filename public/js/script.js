e28ca2ad073e0fd55312344a6cff8eb906c16a12
Shreedhar Chavan <meshree4@gmail.com>
2022-11-14 22:51:02 +0530

    Can delete a todo

--- public/js/script.js
+++ public/js/script.js
@@ -13,3 +13,17 @@ const updateTodo = (id) => {
     })
     .catch((err) => console.error(err));
 };
+
+// eslint-disable-next-line no-unused-vars
+const deleteTodo = (id) => {
+  fetch(`/todos/${id}`, {
+    method: "delete",
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((res) => {
+      if (res.ok) {
+        window.location.reload();
+      }
+    })
+    .catch((err) => console.error(err));
+};
